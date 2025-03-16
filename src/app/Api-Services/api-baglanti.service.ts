1f0d46589d4b3c2f0a0e07f835d9efad4655848e
Mehmet Karakoz <mehmetkarakoz0@gmail.com>
2025-03-16 23:11:27 +0300

    tailwind + Added to html

--- src/app/Api-Services/api-baglanti.service.ts
+++ src/app/Api-Services/api-baglanti.service.ts
@@ -1,7 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Todo} from '../app.types/app.types'
+import {Todo} from '../Model/app.types'
 
 @Injectable({
   providedIn: 'root'
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {TodosStore} from './store/app.store'
 import {JsonPipe} from '@angular/common';
 import {TableModule} from 'primeng/table';
+import {InputText} from 'primeng/inputtext';
+import {ToggleSwitch} from 'primeng/toggleswitch';
+import {Button} from 'primeng/button';
+import {Checkbox} from 'primeng/checkbox';
 @Component({
   selector: 'app-root',
-  imports: [JsonPipe, TableModule],
+  imports: [JsonPipe, TableModule, InputText, ToggleSwitch, Button, Checkbox],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Signal_Store';
   todosStore = inject(TodosStore)
-
+  openFilter=signal(false);
+userId=[1,3,5,7]
   ngOnInit(): void {
     this.todosStore.getTodosObservable();
+  }
+  filter(){
+    this.openFilter.set(!this.openFilter());
   }
 }
