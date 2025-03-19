57c63765f0d084e5449d6c194c3fb21a58db8c20
mehmet.karakoz <mehmetkarakoz0@gmail.com>
2025-03-19 17:55:05 +0300

    getDialog()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {TodosStore} from './store/app.store'
 import {JsonPipe} from '@angular/common';
 import {TableModule} from 'primeng/table';
-import {InputText} from 'primeng/inputtext';
-import {ToggleSwitch} from 'primeng/toggleswitch';
 import {Button} from 'primeng/button';
-import {Checkbox} from 'primeng/checkbox';
 import {TodoHeader, TodoStatus} from './type-model/app.types';
 import {IconField} from 'primeng/iconfield';
 @Component({
@@ -15,16 +12,9 @@ import {IconField} from 'primeng/iconfield';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Signal_Store';
   todosStore = inject(TodosStore)
-  openFilter=signal(false);
-userId=[1,3,5,7]
-  ngOnInit(): void {
-    this.todosStore.getTodosObservable();
-  }
-  filter(){
-    this.openFilter.set(!this.openFilter());
-  }
+  dialog=this.todosStore.getDialog()
+
 
   protected readonly TodoStatus = TodoStatus;
   protected readonly TodoHeader = TodoHeader;
